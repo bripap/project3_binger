32701003a226d2086f2f37bc1a44a2ba38c49f86
bripap <bpappas302@gmail.com>
2022-02-26 11:23:28 -0500

    updated some UI

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -92,7 +92,7 @@ const SearchBooks = () => {
     <>
       <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Search for Books!</h1>
+          <h1>Search for TV shows or movies!</h1>
           <Form onSubmit={handleFormSubmit}>
             <Form.Row>
               <Col xs={12} md={8}>
@@ -102,12 +102,12 @@ const SearchBooks = () => {
                   onChange={(e) => setSearchInput(e.target.value)}
                   type="text"
                   size="lg"
-                  placeholder="Search for a book"
+                  placeholder="Search by title"
                 />
               </Col>
               <Col xs={12} md={4}>
                 <Button type="submit" variant="success" size="lg">
-                  Submit Search
+                  Submit Search 🔍
                 </Button>
               </Col>
             </Form.Row>
@@ -119,7 +119,7 @@ const SearchBooks = () => {
         <h2>
           {searchedBooks.length
             ? `Viewing ${searchedBooks.length} results:`
-            : 'Search for a book to begin'}
+            : 'Search for a TV show or movie to begin'}
         </h2>
         <CardColumns>
           {searchedBooks.map((book) => {
