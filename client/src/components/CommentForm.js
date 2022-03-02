960ab8745fc070807c312dc4cfb8249bc53a1a30
Ladislav Jerabek <ljerabek@microstrategy.com>
2022-03-02 01:58:40 +0100

    Creatgin CommentForm component

---
+++ client/src/components/CommentForm.js
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import {
+    Button,
+    Form,
+} from 'react-bootstrap';
+
+export const CommentForm = ({ book: { bookId, review, rating, watched }, handleUpdateComment }) => {
+  const [comment, setComment] = useState({
+    review: review ? review : '',
+    rating: rating >= 0 ? rating : 0,
+    watched: watched ? watched : false,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, checked, value } = e.target;
+
+    setComment({
+        ...comment,
+        [name]: e.target.matches('input.form-check-input') ? checked : value
+    });
+  }
+
+  return (
+    <>
+      <div className="form-group">
+        <label htmlFor="review1">Review:</label>
+        <Form.Control
+          name="review"
+          id="review1"
+          value={comment.review}
+          onChange={handleInputChange}
+          type="textarea"
+          rows="3"
+          className="form-control rounded-0"
+        />
+      </div>
+
+      <br></br>
+      <label htmlFor="rating1">Rating - Enter (0 - 10) Value:</label>
+      <Form.Control
+        name="rating"
+        id="rating1"
+        value={comment.rating}
+        onChange={handleInputChange}
+        type="number"
+        min="0"
+        max="10"
+      />
+
+      <br></br>
+      <Form.Check
+        label="Watched"
+        name="watched"
+        checked={comment.watched}
+        onChange={handleInputChange}
+      />
+
+      <br></br>
+      <Button onClick={() => handleUpdateComment(bookId, comment) }>
+        Update Watch List
+      </Button>
+    </>
+  );
+};
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,49 +1,28 @@
-import React, { useState, useEffect } from 'react';
-
 import {
   Jumbotron,
   Container,
   CardColumns,
   Card,
   Button,
-  Form,
-} from 'react-bootstrap';
+} from "react-bootstrap";
 
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-import { removeBookId } from '../utils/localStorage';
-import { SAVE_BOOK } from '../utils/mutations';
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import { removeBookId } from "../utils/localStorage";
+import { SAVE_BOOK } from "../utils/mutations";
 
-import '../pages/pages.css';
+import "../pages/pages.css";
 
-import Auth from '../utils/auth';
+import { CommentForm } from "../components/CommentForm";
+import Auth from "../utils/auth";
 
 const SavedBooks = () => {
-  
-
-  // create state for holding our search field data
-  const [reviewInput, setReviewInput] = useState('');
-  const [ratingInput, setRatingInput] = useState('0');
-  const [watchedInput, setWatchedInput] = useState(false);
-  const [useReviewDBValue, setUseReviewDBValue] = useState(false);
-  const [useRatingDBValue, setUseRatingDBValue] = useState(false);
-  const [useWatchedDBValue, setUseWatchedDBValue] = useState(false);
-
-
-
   const { loading, data } = useQuery(QUERY_ME);
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
   const [saveBook, { error1 }] = useMutation(SAVE_BOOK);
 
   const userData = data?.me || {};
-
-  useEffect(() => {
-    setUseReviewDBValue(true);
-    setUseRatingDBValue(true);
-    setUseWatchedDBValue(true);
-  },[]);
-
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -66,72 +45,9 @@ const SavedBooks = () => {
     }
   };
 
-  const HandleReviewInput = async (value) => {
-    setUseReviewDBValue(false);
-    setReviewInput(value);
-  };
-
-  const handleUpdateWatchList = async (bookId) => {
-    let updatedBook = null;
-    let nIndex = 0;
-    let bookFound = false;
-
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-    
-    try {
-
-      for (nIndex = 0; nIndex < userData.savedBooks.length; nIndex++) {
-        if (userData.savedBooks[nIndex].bookId === bookId) {
-
-          const ratingValue = parseInt(ratingInput);
-
-          // Create a book object and set the variables for the book that matches the bookId value passed in.
-          updatedBook = {
-            bookId: bookId,
-            title: userData.savedBooks[nIndex].title,
-            authors: userData.savedBooks[nIndex].authors,
-            description: userData.savedBooks[nIndex].description,
-            image: userData.savedBooks[nIndex].image,
-            review: reviewInput,
-            rating: ratingValue,
-            watched: watchedInput,
-          };  
-          
-          // Set the bookFound variable to true;
-          bookFound = true;
-
-          // Found the book so set the nIndex value so we exit loop.
-          nIndex = userData.savedBooks.length;
-        }
-      }
-
-      // If the book was not found then just return false.
-      if (bookFound === false) {
-        return false;
-      }
-
-      // What I want to do first is to remove the current book.
-      const { data } = await removeBook({
-        variables: { bookId },
-      });
-
-      // Save the new one that has been changed.
-      const { data2 } = await saveBook({
-        variables: { bookData: { ...updatedBook } },
-      });        
-  
-    } catch (err) {
-      console.error(err);
-    }  
-  };
-
-  if (loading) {
-    return <h2>LOADING...</h2>;
+  const handleUpdateComment = (bookId, updatedComment) => {
+    console.log(bookId);
+    console.log(updatedComment);
   }
 
   return (
@@ -142,88 +58,48 @@ const SavedBooks = () => {
         </Container>
       </Jumbotron>
       <Container>
-        <h2>
-          {userData.savedBooks?.length
-            ? `${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? 'movies and tv shows' : 'movies and tv shows'
-              }:`
-            : 'You have nothing saved to your watchlist ☹'}
-        </h2>
-        <CardColumns>
-          {userData.savedBooks?.map((book) => {
-
-            return (
-              <Card key={book.bookId} border="dark">
-                {book.image ? (
-                  <Card.Img
-                    src={book.image}
-                    alt={`The image for ${book.title}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className="small">Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  <Button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeleteBook(book.bookId)}
-                  >
-                    Delete from watchlist
-                  </Button>
-
-                  {/* New controls added by Duane */}
-                  
-                  
-                  <br></br>  
-                  <div class="form-group">
-                    <label htmlFor="review1">Review:</label>                                  
-                    <Form.Control
-                      name="reviewInput"
-                      id="review1"
-                      value={useReviewDBValue ? book.review : reviewInput}
-                      onChange={(e) => HandleReviewInput(e.target.value)}
-                      type="textarea"
-                      rows="3"
-                      class="form-control rounded-0"
+        {loading ? (
+          <div>Books are still loading</div>
+        ) : (
+          <div>
+            <h2>
+              {userData.savedBooks?.length
+                ? `${userData.savedBooks.length} saved ${
+                    userData.savedBooks.length === 1
+                      ? "movies and tv shows"
+                      : "movies and tv shows"
+                  }:`
+                : "You have nothing saved to your watchlist ☹"}
+            </h2>
+            <CardColumns>
+              {userData.savedBooks.map(book =>
+                <Card key={book.bookId} border="dark">
+                  {book.image ? (
+                    <Card.Img
+                      src={book.image}
+                      alt={`The image for ${book.title}`}
+                      variant="top"
                     />
-                  </div>
-                  
-                  
+                  ) : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className="small">Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    <Button
+                      className="btn-block btn-danger"
+                      onClick={() => handleDeleteBook(book.bookId)}
+                    >
+                      Delete from watchlist
+                    </Button>
 
-                  <br></br>  
-                  <label htmlFor="rating1">Rating - Enter (0 - 10) Value:</label>    
-                  <Form.Control
-                    name="ratingInput"
-                    id="rating1"
-                    value={useRatingDBValue ? book.rating : ratingInput}
-                    onChange={(e) => setRatingInput(e.target.value)}
-                    type="number"
-                    min= "0"
-                    max= "10"
-                  />    
-
-                  <br></br>                               
-                  <Form.Check
-                    label="Watched" 
-                    name="watchedInput" 
-                    id="check1" 
-                    checked={useWatchedDBValue ? book.watched : watchedInput}
-                    onChange={(e) => setWatchedInput(e.target.checked)} 
-       
-                  />
-                  
-                  <br></br>  
-                  <Button
-                    onClick={() => handleUpdateWatchList(book.bookId)} >
-                    Update Watch List
-                  </Button>
-
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardColumns>
+                    <br></br>
+                    <CommentForm book={book} handleUpdateComment={handleUpdateComment} />
+                  </Card.Body>
+                </Card>
+              )}
+            </CardColumns>
+          </div>
+        )}
       </Container>
     </>
   );
