8716d00e7a13e2692fc7f65bb82b7dafb40dafc1
Duane Cantera <duane.cantera@comcast.net>
2022-03-01 20:39:57 -0500

    Started to work on putting some of my file changes in Lada's branch.

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -45,10 +45,94 @@ const SavedBooks = () => {
     }
   };
 
-  const handleUpdateComment = (bookId, updatedComment) => {
-    console.log(bookId);
-    console.log(updatedComment);
-  }
+  //////////////////////////////////////////////////////////////////////
+  // LADA'S VERSION:
+  //////////////////////////////////////////////////////////////////////
+  //const handleUpdateComment = (bookId, updatedComment) => {
+  //  console.log(bookId);
+  //  console.log(updatedComment);
+  //}
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // This functions deals with the Update Watch List operation.
+  // It first finds the watch list item and saves the values.
+  // It then deletes the current one and adds a new one with the new values.
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  const handleUpdateComment = async (bookId, updatedComment) => {
+
+    let updatedBook = null;
+    let nIndex = 0;
+    let bookFound = false;
+    let ratingValue = 0;
+  
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+  
+    if (!token) {
+      return false;
+    }
+      
+    try {
+  
+      // Loop through the saved watched items looking for a match on the bookId value passed
+      // in to this function.
+      for (nIndex = 0; nIndex < userData.savedBooks.length; nIndex++) {
+  
+        // If the bookId matches then perform the following operations.
+        if (userData.savedBooks[nIndex].bookId === bookId) {
+  
+          // If we don't have a value then set it to zero.
+          //if (ratingInput.length === 0) {
+          if (updatedComment.rating.length === 0) {
+            ratingValue = 0;
+            updatedComment.rating = 0;
+          }
+          // Else convert it to an integer value.
+          else {
+              ratingValue = parseInt(updatedComment.rating);
+          }
+  
+          // Create a book object and set the variables for the book that matches the bookId value passed in.
+          updatedBook = {
+            bookId: bookId,
+            title: userData.savedBooks[nIndex].title,
+            authors: userData.savedBooks[nIndex].authors,
+            description: userData.savedBooks[nIndex].description,
+            image: userData.savedBooks[nIndex].image,
+            review: updatedComment.review,
+            rating: ratingValue,
+            watched: updatedComment.watched,
+          };  
+            
+          // Set the bookFound variable to true;
+          bookFound = true;
+  
+          // Found the book so set the nIndex value so we exit loop.
+          nIndex = userData.savedBooks.length;
+        }
+      }
+  
+      // If the book was not found then just return false.
+      if (bookFound === false) {
+        return false;
+      }
+  
+      // What I want to do first is to remove the current book.
+      // eslint-disable no-unused-vars 
+      const { data } = await removeBook({
+        variables: { bookId },
+      });
+  
+      // Save the new one that has been changed.
+      // eslint-disable no-unused-vars 
+      const { data2 } = await saveBook({
+        variables: { bookData: { ...updatedBook } },
+      });        
+    
+    } catch (err) {
+      console.error(err);
+    }  
+  };
 
   return (
     <>
